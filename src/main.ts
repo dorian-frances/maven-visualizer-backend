797ed7746ba29966310bbb4c7fda9522257e1c6c
dorian-frances <dfrances.pro@gmail.com>
2025-03-14 18:28:02 +0100

    chore: enable local cors on backend

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './bootstrap/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+  await app.listen(process.env.PORT ?? 8081);
 }
 bootstrap();
